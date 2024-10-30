fff89464848ebf939ffd2c08b5921db294527f06
JackStandley <jspr5@umsystem.edu>
2024-10-30 14:13:46 -0500

    Created a 'Create' page to allow the user to create posts

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect, abo
 # make a Flask application object called app
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
+app.config['SECRET_KEY'] = 'your secret key'
 
 
 # Function to open a connection to the database.db file
@@ -29,18 +29,39 @@ def index():
     #execute a query to read all posts from the post table in the database
     posts = conn.execute('SELECT * FROM posts').fetchall()
 
-
-
     #close the connection
     conn.close()
 
     #send the posts to the index.html template to be displayted
     
-    
     return render_template('index.html', posts = posts)
 
 
 # route to create a post
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    #determine if the page is being requested with a POST or GET request
+    if request.method == 'POST':
+        #get the title and content that was submitted
+        title = request.form['title']
+        content = request.form['content']
+
+
+        #display an error message if title or content is not submitted
+        #else make a database connection and insert the blog post content
+        if not title:
+            flash("Title is required")
+        elif not content:
+            flash('Content is required')
+        else:
+            conn = get_db_connection()
+            #insert data into database
+            conn.execute('INSERT INTO posts (title, content) VALUES (?,?)', (title,content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
 
 
 app.run(port=5008)
